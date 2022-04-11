3ee1af72896a05402803b845be610ff672ef6061
marxchryz <delmundo_marxchryz@yahoo.com>
2022-04-12 01:40:06 +0800

    tabnabbing lab updated

--- reverse-tabnabbing/caila.js
+++ reverse-tabnabbing/caila.js
@@ -2,20 +2,24 @@ class CAILA {
   constructor({ whitelistedDomains = [] }) {
     this.whitelistedDomains = whitelistedDomains;
   }
+
+  secureAnchorTag(anchorTag) {
+    /* only edit below this line */
+
+    // get hostname so we can compare later in the blacklisted domains
+    let hostname = anchorTag.hostname;
+
+    anchorTag.setAttribute('rel', 'noopener nofollow noreferrer');
+
+    /* only edit above this line */
+    return anchorTag;
+  }
+
   protect() {
     let anchorTags = document.querySelectorAll('a[target=_blank]');
 
     for (let i = 0; i < anchorTags.length; i++) {
-      /* only edit below this line */
-
-      // get hostname so we can compare later in the blacklisted domains
-      let hostname = anchorTags[i].hostname;
-
-      anchorTags[i].setAttribute('rel', 'noopener nofollow noreferrer');
-
-      /* only edit above this line*/
+      secureAnchorTag(anchorTags[i]);
     }
-
-    return anchorTags;
   }
 }
